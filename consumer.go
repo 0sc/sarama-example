966fe063058d75c83e3ab516187b4f96eb0643b4
Osmond Oscar <osmond.oranagwa@andela.com>
2016-08-22 23:17:09 +0100

    add sample consumer

---
+++ consumer.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sarama"
+)
+
+func subscribe(topic string, consumer sarama.Consumer) {
+	partitionList, err := consumer.Partitions(topic) //get all partitions on the given topic
+	if err != nil {
+		fmt.Println("Error retrieving partitionList ", err)
+	}
+	initialOffset := sarama.OffsetOldest //get offset for the oldest message on the topic
+
+	for _, partition := range partitionList {
+		pc, _ := consumer.ConsumePartition(topic, partition, initialOffset)
+
+		go func(pc sarama.PartitionConsumer) {
+			for message := range pc.Messages() {
+				messageReceived(message)
+			}
+		}(pc)
+	}
+}
+
+func messageReceived(message *sarama.ConsumerMessage) {
+	saveMessage(string(message.Value))
+}
