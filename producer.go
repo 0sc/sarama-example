eb2371ccf833c4a1eaba32c7d0fb9fd4ea16550e
Osmond Oscar <osmond.oranagwa@andela.com>
2016-08-22 23:16:55 +0100

    add sample producer

---
+++ producer.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/sarama"
+
+var brokers = []string{"127.0.0.1:9092"}
+
+func newProducer() (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	producer, err := sarama.NewSyncProducer(brokers, config)
+
+	return producer, err
+}
+
+func prepareMessage(topic, message string) *sarama.ProducerMessage {
+	msg := &sarama.ProducerMessage{
+		Topic:     topic,
+		Partition: -1,
+		Value:     sarama.StringEncoder(message),
+	}
+
+	return msg
+}
